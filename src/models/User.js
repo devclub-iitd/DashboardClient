c9eb3fd9e9850c1dbd99375943e4dda1c39dfa9c
Suyash Agrawal <ozym4nd145@outlook.com>
2019-04-09 21:21:33 +0530

    Added models

---
+++ src/models/User.js
@@ -0,0 +1,54 @@
+const defaultUser = {
+    _id: "",
+    email: "",
+    name: "",
+    entryNumber: "",
+    hostel: "",
+    gender: "",
+    joinYear: new Date(),
+    gradYear: new Date(),
+    birthDate: new Date(),
+    mobileNumber: "",
+    hometown: "",
+    interests: "",
+    specialization: "",
+    intro: "",
+    displayOnWebsite: false,
+    links: {},
+    createdAt: new Date(),
+    updatedAt: new Date()
+};
+
+class User {
+    constructor(user=defaultUser) {
+        this.id = user._id;
+        this.email = user.email;
+        this.name = user.name;
+        this.entryNumber = user.entryNumber;
+        this.hostel = user.hostel;
+        this.gender = user.gender;
+        this.joinYear = new Date(user.joinYear);
+        this.gradYear = new Date(user.gradYear);
+        this.birthDate = new Date(user.birthDate);
+        this.mobileNumber = user.mobileNumber;
+        this.hometown = user.hometown;
+        this.interests = user.interests;
+        this.specialization = user.specialization;
+        this.intro = user.intro;
+        this.displayOnWebsite = user.displayOnWebsite;
+        this.links = user.links;
+        this.createdAt = new Date(user.createdAt);
+        this.updatedAt = new Date(user.updatedAt);
+    }
+
+    static parse(users) {
+        let userObjs = [];
+        for (let i = 0; i < users.length; i = i + 1) {
+            userObjs.push(new User(users[i]));
+        }
+        userObjs.sort((first,second)=>(second.gradYear - first.gradYear));
+        return userObjs;
+    }
+};
+
+export default User;
